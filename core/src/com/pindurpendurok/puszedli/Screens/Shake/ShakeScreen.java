3c15e3619692887db48c838df35f00272199c5e8
Martin Horváth <pigfighter2@gmail.com>
2020-01-21 21:31:41 +0100

    Shake alap koncepció. Innentől egy if majd bekalibráljuk

---
+++ core/src/com/pindurpendurok/puszedli/Screens/Shake/ShakeScreen.java
@@ -0,0 +1,32 @@
+package com.pindurpendurok.puszedli.Screens.Shake;
+
+import hu.csanyzeg.master.MyBaseClasses.Assets.AssetList;
+import hu.csanyzeg.master.MyBaseClasses.Game.MyGame;
+import hu.csanyzeg.master.MyBaseClasses.Scene2D.MyScreen;
+
+public class ShakeScreen extends MyScreen {
+
+    public static AssetList list = new AssetList();
+    static {
+        AssetList.collectAssetDescriptor(ShakeStage.class, list);
+    }
+
+    public ShakeScreen(MyGame game) {
+        super(game);
+    }
+
+    @Override
+    protected void afterAssetsLoaded() {
+
+    }
+
+    @Override
+    public AssetList getAssetList() {
+        return null;
+    }
+
+    @Override
+    public void init() {
+
+    }
+}
---
+++ core/src/com/pindurpendurok/puszedli/Screens/Shake/ShakeStage.java
@@ -0,0 +1,31 @@
+package com.pindurpendurok.puszedli.Screens.Shake;
+
+import com.badlogic.gdx.Gdx;
+
+import hu.csanyzeg.master.MyBaseClasses.Assets.AssetList;
+import hu.csanyzeg.master.MyBaseClasses.Game.MyGame;
+import hu.csanyzeg.master.MyBaseClasses.Scene2D.MyStage;
+import hu.csanyzeg.master.MyBaseClasses.Scene2D.ResponseViewport;
+
+public class ShakeStage extends MyStage {
+    public static AssetList assetList = new AssetList();
+    static {
+
+    }
+
+    private final float GRAVITY_EARTH = 9.807f;
+
+    public ShakeStage(MyGame game) {
+        super(new ResponseViewport(720), game);
+
+        float xGrav = Gdx.input.getAccelerometerX() / GRAVITY_EARTH;
+        float yGrav = Gdx.input.getAccelerometerY() / GRAVITY_EARTH;
+        float zGrav = Gdx.input.getAccelerometerZ() / GRAVITY_EARTH;
+
+        // gForce will be close to 1 when there is no movement.
+        float gForce = (float)Math.sqrt((xGrav * xGrav) + (yGrav * yGrav) + (zGrav * zGrav));
+
+        
+
+    }
+}
