31ff510da7e87cb79c69feafdd54b4932d765a14
marton552 <marton552@gmail.com>
2020-01-26 22:16:37 +0100

    GameStage alakul UUU LEHAGYOM A BENDZSI PUSH SZÁMAIT

--- core/src/com/pindurpendurok/puszedli/Screens/Game/GameStage.java
+++ core/src/com/pindurpendurok/puszedli/Screens/Game/GameStage.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.pindurpendurok.puszedli.Elements.SimpleButton;
 import com.pindurpendurok.puszedli.Screens.Actors.CircleAtBackgroundActor;
+import com.pindurpendurok.puszedli.Screens.Actors.StatusBarActor;
 import com.pindurpendurok.puszedli.Screens.Classes.Date;
 import com.pindurpendurok.puszedli.Screens.Favago.FavagoScreen;
 import com.pindurpendurok.puszedli.Screens.Foci.FociScreen;
@@ -15,6 +16,7 @@ import com.pindurpendurok.puszedli.Screens.JobsGame.PapWorldScreen;
 import com.pindurpendurok.puszedli.Screens.JobsGame.PszichiaterScreen;
 import com.pindurpendurok.puszedli.Screens.MiniGame.MathGameScreen;
 import com.pindurpendurok.puszedli.Screens.MiniGame.MathMenuStage;
+import com.pindurpendurok.puszedli.Screens.Rocking.RockingScreen;
 import com.pindurpendurok.puszedli.Screens.Shake.ShakeScreen;
 import com.pindurpendurok.puszedli.Screens.Trash.TrashScreen;
 
@@ -33,7 +35,10 @@ public class GameStage extends SimpleWorldStage {
 
     public final static String BACKGROUND = "mainback.png";
     public final static String FONT = "Bosk.otf";
-
+    static final String[] HATTEREK = new String[]{"elemek/szoba/szoba1.png","elemek/szoba/szoba2.png","elemek/szoba/szoba3.png","elemek/szoba/szoba4.png","elemek/szoba/szoba5.png"};
+    static final String[] BENDZSIK = new String[]{"elemek/dave/Davey.png","elemek/dave/DaveyAlaMexican.png","elemek/dave/DaveyIgen.png","elemek/dave/DaveyVaják.png","elemek/dave/gangsterDavey.png",
+            "elemek/dave/jasonDavey.png","elemek/dave/kawaiiDavey.png","elemek/dave/marioDavey.png","elemek/dave/NudiDavey.png","elemek/dave/papDavey.png",
+            "elemek/dave/rendőrDavey.png","elemek/dave/tündérDavey.png","elemek/dave/zacseszDavey.png","elemek/dave/ZoldDavey.png","elemek/dave/matrixDavey.png"};
     public boolean timer_able_to_count = true;
     public int ticks = 0;
     public int count = 0;
@@ -51,6 +56,12 @@ public class GameStage extends SimpleWorldStage {
         AssetList.collectAssetDescriptor(CircleAtBackgroundActor.class, assetList);
         assetList.addFont(FONT, 60, Color.WHITE).protect = true;
         assetList.addTexture(BACKGROUND);
+        for (int i = 0; i < HATTEREK.length; i++) {
+            assetList.addTexture(HATTEREK[i]).protect = true;
+        }
+        for (int i = 0; i < BENDZSIK.length; i++) {
+            assetList.addTexture(BENDZSIK[i]).protect = true;
+        }
     }
 
 
@@ -73,14 +84,21 @@ public class GameStage extends SimpleWorldStage {
             //Pap minigame
             save.putInteger("papkell",0);
             save.putInteger("papmegvan",0);
+            save.putInteger("hatter",0);
+            save.putInteger("dave",0);
         }
         save.flush();
         datum.leptetes();
 
 
-        OneSpriteStaticActor BackGround = new OneSpriteStaticActor(game, BACKGROUND);
+        OneSpriteStaticActor BackGround = new OneSpriteStaticActor(game, HATTEREK[save.getInteger("hatter")]);
         BackGround.setSize(getWidth(),getHeight());
         addActor(BackGround);
+
+        OneSpriteStaticActor Dave = new OneSpriteStaticActor(game, BENDZSIK[save.getInteger("dave")]);
+        Dave.setSize(getWidth()/1.5f,getHeight()/1.5f);
+        Dave.setPosition(getWidth()/2-Dave.getWidth()/2,getHeight()/40);
+        addActor(Dave);
 
         naptar = new MyLabel(game, "Ez itt a datum", ls) {
             @Override
@@ -89,11 +107,14 @@ public class GameStage extends SimpleWorldStage {
             }
         };
         naptar.setPosition(0,getHeight()-naptar.getHeight());
+        naptar.setColor(Color.BLACK);
         naptar.setFontScale(1.5f);
         addActor(naptar);
 
-        CircleAtBackgroundActor asd = new CircleAtBackgroundActor(game,world);
-        addActor(asd);
+        StatusBarActor bar = new StatusBarActor(game,this,"red",getWidth()/1.5f,getHeight()/30,100,100,2.5f,"Éhség");
+        StatusBarActor bar2 = new StatusBarActor(game,this,"green",getWidth()/1.5f,getHeight()/30,100,100,4,"Szomjúság");
+        StatusBarActor bar3 = new StatusBarActor(game,this,"blue",getWidth()/1.5f,getHeight()/30,100,100,5.5f,"Stressz");
+        StatusBarActor bar4 = new StatusBarActor(game,this,"gold",getWidth()/1.5f,getHeight()/30,100,100,7,"Véralkohol szint");
 
 
         addTimer(new TickTimer(0, true, new TickTimerListener() {
@@ -192,5 +213,16 @@ public class GameStage extends SimpleWorldStage {
             }
         });
         addActor(favagas);
+
+        SimpleButton rock = new SimpleButton(game, "ringatas");
+        rock.setX(350);
+        rock.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                game.setScreen(new RockingScreen(game));
+            }
+        });
+        addActor(rock);
     }
 }
