80a7b299ff543e177d548fae1ead7d4295388504
Martin Horváth <pigfighter2@gmail.com>
2020-01-30 21:50:50 +0100

    Loading cucc

--- core/src/com/pindurpendurok/puszedli/Screens/GameLoadingStage.java
+++ core/src/com/pindurpendurok/puszedli/Screens/GameLoadingStage.java
@@ -1,25 +1,40 @@
 package com.pindurpendurok.puszedli.Screens;
 
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import com.pindurpendurok.puszedli.Elements.ElementAssets;
+import com.pindurpendurok.puszedli.Elements.SimpleLabel;
 
 import hu.csanyzeg.master.MyBaseClasses.Assets.AssetList;
 import hu.csanyzeg.master.MyBaseClasses.Assets.LoadingStage;
 import hu.csanyzeg.master.MyBaseClasses.Game.MyGame;
 import hu.csanyzeg.master.MyBaseClasses.Scene2D.OneSpriteStaticActor;
 import hu.csanyzeg.master.MyBaseClasses.Scene2D.ResponseViewport;
+import hu.csanyzeg.master.MyBaseClasses.UI.MyLabel;
 
 public class GameLoadingStage extends LoadingStage {
-    public final static String BACK = "loading.jpg";
+    public final static String FONTOCSKA = "alegreyaregular.otf";
+
     public static AssetList assetList = new AssetList();
     static {
-        assetList.addTexture(BACK).protect = true;
+        assetList.addFont(FONTOCSKA, 80).protect = true;
     }
+    SimpleLabel dolog;
+
 
     public GameLoadingStage(MyGame game) {
         super(new ResponseViewport(720), game);
-        OneSpriteStaticActor BackGround = new OneSpriteStaticActor(game, BACK);
-        BackGround.setSize(getWidth(),getHeight());
-        addActor(BackGround);
+
+        dolog = new SimpleLabel(game, "SZIA", FONTOCSKA);
+        dolog.setAlignment(Align.center);
+        dolog.setPosition(getViewport().getWorldWidth() / 2 - dolog.getWidth() / 2, getViewport().getWorldHeight() / 2 - dolog.getHeight() / 2);
+        addActor(dolog);
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+        dolog.setText(getPercent()+"%");
     }
 
     @Override
